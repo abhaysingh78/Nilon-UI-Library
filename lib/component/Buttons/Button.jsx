c69021cdc7c36e668b3dff59e8f86cbc08cd2eb9
unknown <abhay112233chauhan@gmail.com>
2023-02-01 21:09:07 +0530

    Added Modal Component

---
+++ lib/component/Buttons/Button.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "../styles/_button.scss";
+
+const Button = props => {
+  const [shadow, setShadow] = useState("");
+  useEffect(() => {
+    switch (props.color) {
+      case "error":
+        setShadow("shadowError");
+        break;
+      case "secondary":
+        setShadow("shadowSecondary");
+        break;
+      case "success":
+        setShadow("shadowSuccess");
+        break;
+      case "warning":
+        setShadow("shadowWarning");
+        break;
+      default:
+        setShadow("shadowPrimary");
+        break;
+    }
+  }, [shadow]);
+
+  return (
+    <>
+      <button
+        className={`${props.color} ${props.size} ${props.shadow && shadow} ${
+          props.rounded && "round"
+        } ${props.bold && "bold"} ${props.light && "light"}`}
+        onClick={props.onClick && props.onClick}
+      >
+        {props.name ? props.name : "Button"}
+      </button>
+    </>
+  );
+};
+export default Button;
